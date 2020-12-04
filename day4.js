f52dfefe94452a0d3d0c67c862c3236bb61461f2
timorohrer <timorohrer@gmail.com>
2020-12-04 14:39:39 +0100

    Fix off by one

--- day4.js
+++ day4.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+//var input = fs.readFileSync("input/day4.txt", "utf8").split("\n\n");
 var input = fs.readFileSync("input/day4.txt", "utf8").split("\n\n");
 
 let expected_fields = [
@@ -49,68 +50,59 @@ function valid(exp, passport) {
         switch (field) {
             case "byr":
                 if (pair[1].length != 4) {
-                    //console.log("byr");
                     return false;
                 } else if (pair[1] < 1920 || pair[1] > 2002) {
-                    //console.log("byr");
                     return false;
                 }
                 break;
             case "iyr":
                 if (pair[1].length != 4) {
-                    //console.log("iyr");
                     return false;
                 } else if (pair[1] < 2010 || pair[1] > 2020) {
-                    //console.log("iyr");
                     return false;
                 }
                 break;
             case "eyr":
                 if (pair[1].length != 4) {
-                    //console.log("eyr");
                     return false;
                 } else if (pair[1] < 2020 || pair[1] > 2030) {
-                    //console.log("eyr");
                     return false;
                 }
                 break;
             case "hgt":
+                var num = pair[1];
                 if (pair[1].includes("cm")) {
-                    pair[1] = pair[1].replace("cm", "");
-                    if (pair[1] < 150 || pair[1] > 193) {
-                        //console.log("hgt");
+                    num = num.replace("cm", "");
+                    if (num < 150 || num > 193) {
                         return false;
                     }
                 } else if (pair[1].includes("in")) {
-                    pair[1] = pair[1].replace("in", "");
-                    if (pair[1] < 59 || pair[1] > 76) {
-                        //console.log("hgt");
+                    num = num.replace("in", "");
+                    if (num < 59 || num > 76) {
                         return false;
                     }
+                    // forgot to disqualify heights with no unit
+                } else {
+                    return false;
                 }
                 break;
             case "hcl":
                 if (pair[1][0] != "#") {
-                    //console.log("hcl");
                     return false;
                 } else if (pair[1].length != 7) {
-                    //console.log("hcl");
                     return false;
                 } else if (!pair[1].match(/^[a-f0-9#]*$/)) {
-                    //console.log("hcl");
                     return false;
                 }
                 break;
             case "ecl":
                 let colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
                 if (!colors.includes(pair[1])) {
-                    //console.log("ecl");
                     return false;
                 }
                 break;
             case "pid":
                 if (pair[1].length != 9) {
-                    //console.log("pid");
                     return false;
                 }
                 break;
@@ -124,14 +116,11 @@ var part2 = 0;
 for (const passport of input) {
     if (present(expected_fields, passport)) {
         part1++;
-
         if (valid(expected_fields, passport)) {
-            //console.log(passport + "\n");
             part2++;
-        } else {
-            ////console.log(passport + "\n");
         }
     }
 }
+
 console.log("Part 1: " + part1);
 console.log("Part 2: " + part2);
