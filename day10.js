cf97737e3056c125c60dd1e521b3365eff3f9363
timorohrer <timorohrer@gmail.com>
2020-12-10 15:50:47 +0100

    Solve part 2 with dp

--- day10.js
+++ day10.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 
 var input = fs.readFileSync("input/day10.txt", "utf8").split("\n").map(Number);
-//var input = fs.readFileSync("input/test.txt", "utf8").split("\n").map(Number);
 
 // sort as numbers
 input.sort(function (a, b) {
@@ -11,27 +10,36 @@ input.sort(function (a, b) {
 // add device's built-in adapter
 input.push(input[input.length - 1] + 3);
 
-var diff1 = 0;
-var diff3 = 0;
-var pow2 = 0;
-var pow7 = 0;
+let diff1 = 0,
+    diff3 = 0;
+
 for (var i = 0; i < input.length; i++) {
     var diff = input[i + 1] - input[i];
-    var neg = (i >=3 ? input[i-3] : null);
     // part 1 checks
     if (diff == 1) {
         diff1++;
     } else if (diff == 3) {
         diff3++;
     }
-    // part 2 checks
-    if (input[i+1] - neg == 4) {
-        pow2 -= 2;
-        pow7++;
-    } else if (input[i+1] - input[i-1] == 2) {
-        pow2++;
-    }
 }
 
-console.log("Part 1: " + (diff1 * diff3)),
-console.log("Part 2: " + (2**pow2*7**pow7));
+let SEEN = new Map();
+function rec(i) {
+    if (i == input.length - 1) {
+        return 1;
+    }
+    if (SEEN.has(i)) {
+        return SEEN.get(i);
+    }
+    var ans = 0;
+    for (var j = i+1; j < input.length; j++) {
+        if (input[j] - input[i] <= 3) {
+            ans += rec(j);
+        }
+    }
+    SEEN.set(i, ans);
+    return ans;
+}
+
+console.log("Part 1: " + diff1 * diff3);
+console.log("Part 2: " + rec(0));
