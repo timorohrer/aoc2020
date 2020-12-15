2a8c32212b89533cdd3cead1334281e93cc8a949
timorohrer <timorohrer@gmail.com>
2020-12-15 21:55:53 +0100

    Clean up day 15

--- day15.js
+++ day15.js
@@ -4,32 +4,19 @@ var input = fs.readFileSync("input/day15.txt", "utf8");
 
 input = input.match(/\d+/g).map(Number);
 
-let last = new Map();
+// last spoken will always be the turn before if it exists
+// last before that is stored in a Map for fast lookups
 
 turn = (n) => {
-    var spoken = 0;
-    for (var i = 0; i < n; i++) {
-        if (i < input.length) {
-            // read out loud starting numbers
-            last.set(input[i], [i + 1]);
-            spoken = input[i];
-        } else {
-            if (last.get(spoken).length == 1) {
-                spoken = 0;
-                try {
-                    last.set(spoken, [i + 1, last.get(spoken)[0]]);
-                } catch (err) {
-                    last.set(spoken, [i + 1]);
-                }
-            } else {
-                spoken = last.get(spoken)[0] - last.get(spoken)[1];
-                try {
-                    last.set(spoken, [i + 1, last.get(spoken)[0]]);
-                } catch (err) {
-                    last.set(spoken, [i + 1]);
-                }
-            }
-        }
+    let last = new Map();
+    for (var i = 0; i < input.length; i++) {
+        last.set(input[i], i);
+    }
+    let spoken = input[input.length - 1];
+    for (var i = input.length; i < n; i++) {
+        let temp = last.has(spoken) ? i - last.get(spoken) - 1 : 0;
+        last.set(spoken, i - 1);
+        spoken = temp;
     }
     return spoken;
 };
